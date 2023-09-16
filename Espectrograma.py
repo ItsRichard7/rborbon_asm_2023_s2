f265747d45b47caa3229d17f12c4886ca96f44a6
ItsRichard7 <ricardobm@estudiantec.cr>
2023-09-16 16:56:36 -0600

    Trazado Espectrograma

--- Espectrograma.py
+++ Espectrograma.py
@@ -9,5 +9,16 @@ def generarSeñalAudio(duracion, radioMuestreo, frecuencia, amplitud):
     señalAudio = amplitud * np.sin(2 * np.pi * frecuencia * t)
     return señalAudio
 
+# Función para trazar el espectrograma de una señal de audio
+def trazarEspectrograma(señalAudio, radioMuestreo):
+    f, t, Sxx = signal.spectrogram(señalAudio, fs=radioMuestreo)
+    plt.figure(figsize=(10, 4))
+    plt.pcolormesh(t, f, 10 * np.log10(Sxx))
+    plt.title('Espectrograma de la Señal de Audio')
+    plt.xlabel('Tiempo (s)')
+    plt.ylabel('Frecuencia (Hz)')
+    plt.colorbar(label='dB')
+    plt.show()
+
 
 
