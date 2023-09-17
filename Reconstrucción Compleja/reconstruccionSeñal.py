d6ea0bee5c09597676ed37f7b7a71a87abe41dca
ItsRichard7 <ricardobm@estudiantec.cr>
2023-09-17 16:42:32 -0600

    Ejemplo con señal senoidal

--- Reconstrucción Compleja/reconstruccionSeñal.py
+++ Reconstrucción Compleja/reconstruccionSeñal.py
@@ -19,5 +19,41 @@ def recuperarSeñal(señalRuido):
     señalFiltrada = np.convolve(señalRuido, np.ones(tamañoFiltro)/tamañoFiltro, mode='same')
     return señalFiltrada
 
+# Graficar las señales original, ruidosa y recuperada
+def graficarSeñales(señalOriginal, señalRuido, señalRecuperada, vectorTiempo):
+    plt.figure(figsize=(12, 6))
+    # Graficar Señal Original
+    plt.subplot(3, 1, 1)
+    plt.title('Señal Original')
+    plt.plot(señalOriginal)
+    # Graficar Señal con Ruido
+    plt.subplot(3, 1, 2)
+    plt.title('Señal Ruidosa')
+    plt.plot(señalRuido)
+    # Graficar Señal Recuperada
+    plt.subplot(3, 1, 3)
+    plt.title('Señal Recuperada')
+    plt.plot(señalRecuperada)
+    # Mostrar las gráficas
+    plt.tight_layout()
+    plt.show()
+
+# Parámetros de la señal
+duracion = 1  # Duración de la señal en segundos
+radioMuestreo = 44100  # Tasa de muestreo en Hz
+frecuencia = 5.0  # Frecuencia en Hz (A4)
+
+# Generar la señal de audio original
+señalOriginal = generarSeñalAudio(duracion, radioMuestreo, frecuencia)
+
+# Configurar el nivel de ruido gaussiano que deseas agregar
+nivelRuido = 0.1
+
+# Agregar ruido gaussiano a la señal original
+señalRuido = agregarRuido(señalOriginal, nivelRuido)
+
+# Recuperar la señal original
+señalRecuperada = recuperarSeñal(señalRuido)
+
 
 
